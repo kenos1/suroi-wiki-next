499d16281b7a980ea9fec356513dafe2a4e47bed
Kenos <75338427+kenos1@users.noreply.github.com>
2024-05-17 21:00:54 -0600

    added math cuz why not

--- wiki/scripts/main.ts
+++ wiki/scripts/main.ts
@@ -1,2 +1,8 @@
 import "./search"
 import "iconify-icon"
+
+import renderMathInElement from "katex/contrib/auto-render"
+
+renderMathInElement(document.body, {
+  delimiters: [{left: "$$", right: "$$", display: false}]
+})
