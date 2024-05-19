90b46972785052fa0b452140fc7b5d239f3a4fd2
Kenos <75338427+kenos1@users.noreply.github.com>
2024-05-19 16:42:03 -0600

    goofy

--- wiki/config.ts
+++ wiki/config.ts
@@ -1,6 +1,6 @@
 export const Config = {
   url: "https://kenos1.github.io/suroi-wiki-next",
-  disableThumbnails: true
+  disableThumbnails: false
 } as {
   url: string,
   disableThumbnails: boolean
