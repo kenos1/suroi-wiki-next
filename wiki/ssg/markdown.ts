f68ef652897b58037b92a3ff3d7ff95e26b37291
Kenos <75338427+kenos1@users.noreply.github.com>
2024-05-18 21:13:53 -0600

    good enough

--- wiki/ssg/markdown.ts
+++ wiki/ssg/markdown.ts
@@ -19,7 +19,7 @@ export async function renderMarkdown(
   path: string,
   options?: RenderMarkdownOptions
 ) {
-  const contentPath = Path.join("./content", path + ".md")
+  const contentPath = Path.join("./content", path + ".md");
   if (!existsSync(contentPath)) return;
   let file = await readFile(contentPath, { encoding: "utf8" });
 
@@ -31,42 +31,52 @@ export async function renderMarkdown(
   return markdownIt.render(file);
 }
 
-export async function createMarkdownPage(title: string, path: string, markdownPath: string) {
+export async function createMarkdownPage(
+  title: string,
+  path: string,
+  markdownPath: string
+) {
   return await createPage(path, {
     title: title,
     path: path,
     description: await getDescription(markdownPath),
-    content: await renderMarkdown(markdownPath) ?? html`<div class="notification is-danger">No Article Found.</div>`
-  })
+    content:
+      (await renderMarkdown(markdownPath)) ??
+      html`<div class="notification is-danger">No Article Found.</div>`,
+  });
 }
 
 export async function createItemArticle(options: {
-  path: string,
-  title: string,
-  markdownPath: string,
-  sidebar: string,
-  thumbnail?: string
+  path: string;
+  title: string;
+  markdownPath: string;
+  sidebar: string;
+  thumbnail?: string;
 }) {
   return await createPage(`/wiki/${options.path}`, {
-      title: options.title,
-      thumbnailImage: options.thumbnail,
-      path: `/wiki/${options.path}`,
-      description: await getDescription(options.markdownPath),
-      content: html`
-        <div class="columns is-desktop">
-          <article class="column is-two-thirds">
-            ${(await renderMarkdown(`${options.markdownPath}`)) ??
-            html`<div class="notification is-danger">No Written Article Found.</div>`}
-          </article>
-          ${options.sidebar}
-        </div>
-      `,
-    })
+    title: options.title,
+    thumbnailImage: options.thumbnail,
+    path: `/wiki/${options.path}`,
+    description: await getDescription(options.markdownPath),
+    content: html`
+      <div class="columns is-desktop">
+        <article class="column is-two-thirds">
+          ${(await renderMarkdown(`${options.markdownPath}`)) ??
+          html`<div class="notification is-danger">
+            No Written Article Found.
+          </div>`}
+        </article>
+        ${options.sidebar}
+      </div>
+    `,
+  });
 }
 
 async function getDescription(markdownPath: string) {
-  const path = Path.join("./content", markdownPath + ".md")
+  const path = Path.join("./content", markdownPath + ".md");
   if (!existsSync(path)) return;
 
-  return encodeURI((await readFile(path, {encoding: "utf8"})).slice(0, 400) + "...")
+  return (await readFile(path, { encoding: "utf8" }))
+    .slice(0, 400)
+    .replaceAll("\"", "&quot;");
 }
